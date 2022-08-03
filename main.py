22e373a1da859a18a31372e07f24ae22f3d90bff
M1lken01 <xmilken01x@gmail.com>
2022-08-03 21:03:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ def rename_simple():
     for file in path.iterdir():
         if file.is_file():
             if file.suffix == ".ytd":
-                new_file = str(file.name)[0:4] + "_diff_" + str(num) + "_" + chr(ord('`')+counter) + "_uni" + file.suffix
+                new_file = file.name[0:4] + "_diff_" + str(num) + "_" + chr(ord('`')+counter) + "_uni" + file.suffix
                 file.rename(path / new_file)
                 counter += 1
             if file.suffix == ".ydd":
-                new_file = str(file.name)[0:4] + "_" + str(num) + "_u" + file.suffix
+                new_file = file.name[0:4] + "_" + str(num) + "_u" + file.suffix
                 file.rename(path / new_file)
     cmds()
 
@@ -38,14 +38,40 @@ def rename_complex():
         exit()
     counter = 1
     for file in path.iterdir():
-        if file.is_file() and str(file.name)[0:4] == slot:
+        if file.is_file() and file.name[0:4] == slot:
             if file.suffix == ".ytd" and file.name[10:13] == numold:
-                new_file = str(file.name)[0:4] + "_diff_" + str(numnew) + "_" + chr(ord('`')+counter) + "_uni" + file.suffix
+                new_file = file.name[0:4] + "_diff_" + str(numnew) + "_" + chr(ord('`')+counter) + "_uni" + file.suffix
                 file.rename(path / new_file)
                 counter += 1
             if file.suffix == ".ydd" and file.name[5:8] == numold:
-                new_file = str(file.name)[0:4] + "_" + str(numnew) + "_u" + file.suffix
+                new_file = file.name[0:4] + "_" + str(numnew) + "_u" + file.suffix
                 file.rename(path / new_file)
+    cmds()
+    
+def rename_rearrange():
+    current_slot = "accs"
+    counter = 1
+    ydd_count = 0
+    ytd_count = 0
+    for file in path.iterdir():
+        if file.is_file():
+            if not current_slot == file.name[0:4]:
+                    current_slot = file.name[0:4]
+                    ydd_count = 0
+                    ytd_count = -1
+            if file.suffix == ".ydd":
+                new_file = file.name[0:4] + "_" + str(f"{ydd_count:03}") + "_u" + file.suffix
+                file.rename(path / new_file)
+                #print(file.name[0:4] + "_" + str(f"{ydd_count:03}") + "_u" + file.suffix)
+                ydd_count += 1
+            if file.suffix == ".ytd":
+                if not chr(ord('`')+counter) == file.name[14:15]:
+                    counter = 1
+                    ytd_count += 1
+                new_file = file.name[0:4] + "_diff_" + str(f"{ytd_count:03}") + "_" + chr(ord('`')+counter) + "_uni" + file.suffix
+                file.rename(path / new_file)
+                #print(file.name[0:4] + "_diff_" + str(f"{ytd_count:03}") + "_" + chr(ord('`')+counter) + "_uni" + file.suffix)
+                counter += 1
     cmds()
     
 def start():
@@ -61,9 +87,10 @@ def start():
     cmds()
         
 def cmds():
-    num = input("'1' for simple rename(rename all), '2' for complex rename(rename selected only)\n")
+    num = input("'1' for simple rename(rename all), '2' for complex rename(rename selected only), '3' for rearrange rename(rename all to lowest to highest)\n")
     if num == "1":rename_simple()
     if num == "2":rename_complex()
+    if num == "3":rename_rearrange()
 
 if __name__ == "__main__":
     start()  
