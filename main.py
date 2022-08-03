42bed4369d1bd3d8c4922b42c2a0372cd611dce2
M1lken01 <xmilken01x@gmail.com>
2022-08-03 16:22:12 +0200

    .

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 import pathlib
 import string
-
-'''def rename(num):
-    path = pathlib.Path('.') / "stuff"    
-    for folder in path.iterdir():
-        if folder.is_dir():
-            counter = 1
-            for file in folder.iterdir():
-                if file.is_file():
-                    new_file = str(file.name)[0:4] + "_diff_" + str(num) + "_" + chr(ord('`')+counter) + "_uni" + file.suffix
-                    file.rename(path / folder.name / new_file)
-                    counter += 1'''
                     
 def rename(num):
     path = pathlib.Path('.') / "stuff"    
@@ -20,20 +9,17 @@ def rename(num):
             new_file = str(file.name)[0:4] + "_diff_" + str(num) + "_" + chr(ord('`')+counter) + "_uni" + file.suffix
             file.rename(path / new_file)
             counter += 1
-
-def setnum():
-    intro()
-    num = input("New number:\n")
-    if(num == "c"):
-        exit()
-    rename(num)
     
-def intro():
+def start():
     print("""
 ░█▀▀░▀█▀░█▀█░█▀▀░░░█▀▄░█░█░█░░░█░█░█▀▄░█▀▀░█▀█░█▀█░█▄█░█▀▀
 ░█░█░░█░░█▀█░▀▀▄░░░█▀▄░█░█░█░░░█▀▄░█▀▄░█▀▀░█░█░█▀█░█░█░█▀▀
 ░▀▀▀░░▀░░▀░▀░▀▀░░░░▀▀░░▀▀▀░▀▀▀░▀░▀░▀░▀░▀▀▀░▀░▀░▀░▀░▀░▀░▀▀▀
 """)
     print("Instructions:\n  -Put the files in ./stuff folder\n  -Enter the number to rename the files to")
+    num = input("New number:\n")
+    if(num == "c"):
+        exit()
+    rename(num)
 
-setnum()    
+start()    
